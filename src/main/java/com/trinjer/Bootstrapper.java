967eb5745cf9fd35abcac81a035e7179ed70a7ef
Andrew Tobilko <andrew.tobilko@gmail.com>
2016-09-17 01:09:15 +0300

    booter

---
+++ src/main/java/com/trinjer/Bootstrapper.java
@@ -0,0 +1,17 @@
+package com.trinjer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+/**
+ *
+ * Created by Andrew Tobilko on 27/07/16.
+ *
+ */
+@SpringBootApplication
+public class Bootstrapper {
+
+    public static void main(String[] args) { run(Bootstrapper.class, args); }
+
+}
